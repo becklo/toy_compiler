c014230293f45b00b7a6a05742340c2785fe6a0b
becklo <loula.beck@gmail.com>
2025-02-05 09:33:10 +0000

    dict definition continued
    Signed-off-by: becklo <loula.beck@gmail.com>

--- scoped_dict/__init__.py
+++ scoped_dict/__init__.py
@@ -1,34 +1,38 @@
 class ScopedDict:
-    scope_level = 0
 
     def __push__(self):
         self.scope_level += 1
+        self.dict[self.scope_level] = dict()
 
     def __pop__(self):
         self.scope_level -= 1   
+        self.dict.pop(self.scope_level, None)
 
     def __init__(self):
-        self = dict()
+        self.dict = dict()
+        self.scope_level = 0
+        self.dict[self.scope_level] = dict()
 
-    def __setitem__(self, name, value):
-        self[self.scope_level][name] = value
+    def __setitem__(self, key, value):
+        self.dict[self.scope_level][key] = value
 
-    def __getitem__(self, name):
-        if self[self.scope_level][name] is not None:
-            return self[self.scope_level][name]
+    def __getitem__(self, key):
+        print(self.dict[self.scope_level][key])
+        if self.dict[self.scope_level][key] is not None:
+            return self.dict[self.scope_level][key]
         else:
             while self.scope_level > 0:
-                if self[self.scope_level][name] is not None:
-                    return self[self.scope_level][name]
+                if self.dict[self.scope_level][key] is not None:
+                    return self.dict[self.scope_level][key]
                 self.scope_level -= 1
-            raise KeyError(f"Variable {name} not found in any scope")
+            raise KeyError(f"Variable {key} not found in any scope")
 
-    def __in__(self, name):
-            if self[self.scope_level][name] is not None:
+    def __in__(self, key):
+            if self.dict[self.scope_level][key] is not None:
                 return True
             else:
                 while self.scope_level > 0:
-                    if self[self.scope_level][name] is not None:
+                    if self.dict[self.scope_level][key] is not None:
                         return True
                     self.scope_level -= 1
                 return False
