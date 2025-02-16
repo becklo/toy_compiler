5db7104055b4d0ab53246d723409c33c4cdbf29f
becklo <loula.beck@gmail.com>
2025-02-16 12:00:27 +0000

    initial test definition
    Signed-off-by: becklo <loula.beck@gmail.com>

--- tests/__init__.py
+++ tests/__init__.py
@@ -2,17 +2,17 @@ import subprocess
 import os
 
 def main():
-    input_path = os.path.join(os.path.dirname(__file__), 'input')
-    expected_output_path = os.path.join(os.path.dirname(__file__), 'expected_output')
+    input_path = os.path.join(os.path.dirname(__file__), 'input/valid')
+    # expected_output_path = os.path.join(os.path.dirname(__file__), 'expected_output')
     for filename in os.listdir(input_path):
         input_file = os.path.join(input_path, filename)
         print(input_file)
-        result = subprocess.getoutput(f'cat {input_file} | poetry run parser')
+        result = subprocess.getoutput(f'cat {input_file} | poetry run compiler')
         print(result)
-        expected_output_file = os.path.join(expected_output_path, filename)
-        with open(expected_output_file, 'r') as f:
-            expected_output = f.read()
-        assert result == expected_output
+        # expected_output_file = os.path.join(expected_output_path, filename)
+        # with open(expected_output_file, 'r') as f:
+        #     expected_output = f.read()
+        # assert result == expected_output
 
 if __name__ == '__main__':
     main()
