2aa7586a1032ef9a9d87ad435763bfa5e95672da
becklo <loula.beck@gmail.com>
2025-03-18 21:17:33 +0000

    updated test to compute result value
    Signed-off-by: becklo <loula.beck@gmail.com>

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,9 +1,45 @@
 import subprocess
 import os
 
+from ctypes import CFUNCTYPE, c_int
+
+import llvmlite.binding as llvm
+
 from comp import compile
 
+def create_execution_engine():
+    """
+    Create an ExecutionEngine suitable for JIT code generation on
+    the host CPU.  The engine is reusable for an arbitrary number of
+    modules.
+    """
+    # Create a target machine representing the host
+    target = llvm.Target.from_default_triple()
+    target_machine = target.create_target_machine()
+    # And an execution engine with an empty backing module
+    backing_mod = llvm.parse_assembly("")
+    engine = llvm.create_mcjit_compiler(backing_mod, target_machine)
+    return engine
+
+def compile_ir(engine, llvm_ir):
+    """
+    Compile the LLVM IR string with the given engine.
+    The compiled module object is returned.
+    """
+    # Create a LLVM module object from the IR
+    mod = llvm.parse_assembly(llvm_ir)
+    mod.verify()
+    # Now add the module and make sure it is ready for execution
+    engine.add_module(mod)
+    engine.finalize_object()
+    engine.run_static_constructors()
+    return mod
+
 def main():
+    # All these initializations are required for code generation!
+    llvm.initialize()
+    llvm.initialize_native_target()
+    llvm.initialize_native_asmprinter()  # yes, even this one
 
     input_path = os.path.join(os.path.dirname(__file__), 'input/valid')
     # expected_output_path = os.path.join(os.path.dirname(__file__), 'expected_output')
@@ -12,16 +48,22 @@ def main():
         print(input_file)
 
         try:
-            compile(f'{input_file}.out',open(input_file).read())
+            llvm_ir = compile(f'{input_file}.out',open(input_file).read())
+            engine = create_execution_engine()
+            mod = compile_ir(engine, llvm_ir)
+
+            # Look up the function pointer (a Python int)
+            func_ptr = engine.get_function_address("main")
+
+            # Run the function via ctypes
+            # TODO: make this dependent on the test case
+            cfunc = CFUNCTYPE(c_int)(func_ptr)
+            res = cfunc()
+            print("main(...) =", res)
+            # assert res == 'expected_output'
         except Exception as e:
             print(e)
 
-        # result = subprocess.getoutput(f'cat {input_file} | poetry run compiler')
-        # print(result)
-        # expected_output_file = os.path.join(expected_output_path, filename)
-        # with open(expected_output_file, 'r') as f:
-        #     expected_output = f.read()
-        # assert result == expected_output
 
 if __name__ == '__main__':
     main()
